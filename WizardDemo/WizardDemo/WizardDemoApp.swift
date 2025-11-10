392b6764615bd027e2fc010685dea7bdac6e6a7d
Stef Horner <stef@tedslittlerobot.com>
2025-11-10 11:01:14 +0000

    add demo project

---
+++ WizardDemo/WizardDemo/WizardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardDemoApp.swift
+//  WizardDemo
+//
+//  Created by Stefan Horner on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WizardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
